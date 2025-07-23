ada954708321bf54e44821b1a24c7ebf0db7f55f
André Albuquerque <andre.malbuq@gmail.com>
2025-07-23 17:59:11 +0100

    Feat: Initial FStrings.Logger.info/2 wrapper (still missing tests)

---
+++ lib/f_strings/logger.ex
@@ -0,0 +1,44 @@
+defmodule FStrings.Logger do
+  @moduledoc """
+  Simple Logger wrapper to help using the FStrings library for logs.
+
+  Use it like this:
+
+    iex> use FStrings.Logger
+    iex> abc = 42
+    iex> IO.puts("Uncomment next line, FIX THIS")
+    iex> # Logger.info(~f"This is the meaning of life \#{abc}=")
+    # Will log an info message: "This is the meaning of life 'abc'='42'"
+  ```
+  """
+
+  @no_modifiers []
+
+  require Logger
+  require FStrings
+
+  defmacro __using__(_options) do
+    quote do
+      import FStrings
+      require Logger
+      alias FStrings.Logger, as: Logger
+    end
+  end
+
+  defmacro info(message_or_fun, metadata \\ [])
+
+  defmacro info({:<<>>, _metadata, _concatenated_stuff} = quoted_string, metadata) do
+    quote do
+      evaluated_and_replaced_string =
+        FStrings.sigil_f(unquote(quoted_string), unquote(@no_modifiers))
+
+      Logger.info(evaluated_and_replaced_string, unquote(metadata))
+    end
+  end
+
+  defmacro info(iodata_or_fun, metadata) do
+    quote do
+      Logger.info(unquote(iodata_or_fun), unquote(metadata))
+    end
+  end
+end
